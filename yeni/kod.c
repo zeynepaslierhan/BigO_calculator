6e2e35bd482eabbf154402c706c3a757c7494d84
Asli <200202047@kocaeli.edu.tr>
2022-03-15 16:48:05 +0300

    update kod.c

--- yeni/kod.c
+++ yeni/kod.c
@@ -4,8 +4,8 @@ int main(){
 	int sum = 0;
 	int n=10;
 	int arr[n][n];
-	for (i=0;i<10;i++){
-		for (j=0;j<10;j++){
+	for (i=0;i<n;i++){
+		for (j=0;j<n;j++){
 		    arr[i][j]=i*j;
 			sum = sum + arr[i][j];
 		}
